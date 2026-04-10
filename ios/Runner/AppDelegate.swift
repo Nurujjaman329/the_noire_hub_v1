33c864290b8ae3b4e1b04e898e7dd09cf15c6b91
alfayej55 <mdalfayej017fa@gmail.com>
2026-04-10 08:38:18 +0600

    push ios update
    # Conflicts:
    #	ios/Runner.xcodeproj/project.pbxproj
    #	ios/Runner/AppDelegate.swift
    #	ios/Runner/Info.plist

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,31 @@
 import UIKit
+<<<<<<< HEAD
 import Flutter
 import GoogleMaps
+=======
+import GoogleMaps   // 👈 ADD THIS
+>>>>>>> 91563b6 (push ios update)
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+<<<<<<< HEAD
     GMSServices.provideAPIKey("AIzaSyCrmEOP4JyFCozu7n85BIZqn_8LarJq_iI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+=======
+
+    GMSServices.provideAPIKey("AIzaSyCrmEOP4JyFCozu7n85BIZqn_8LarJq_iI") // 👈 ADD THIS LINE
+
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+>>>>>>> 91563b6 (push ios update)
 }
